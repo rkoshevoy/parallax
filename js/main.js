0d2ed46a8fd395dd5426edcb9dcc85d0963d2eb2
Ruslan Koshevoy <ruslan.koshevoj@gmail.com>
2017-11-16 22:56:24 +0200

    js parallax

--- js/main.js
+++ js/main.js
@@ -17,5 +17,35 @@ window.onload = function() {
 };
 
 $(window).scroll(function() {
-  
+  var block2 = $('#slidingSlow2'),
+      block3 = $('#slidingSlow3'),
+      wTop = $(this).scrollTop(),
+      wHeight = $(this).height(),
+      wBottom = wTop + wHeight,
+      elTop2 = block2.offset().top,
+      elBottom2 = elTop2 + block2.height(),
+      elHeight2 = elBottom2 - elTop2,
+      wElDifferent2 = wBottom - elTop2,
+      elTop3 = block3.offset().top,
+      elBottom3 = elTop3 + block3.height(),
+      elHeight3 = elBottom3 - elTop3,
+      wElDifferent3 = wBottom - elTop3;
+
+      // console.log(wElDifferent);
+
+      $('#slidingSlow1').css({
+        'background-position-y': wTop / 4
+      });
+
+      if (wBottom >= elTop2) {
+        block2.css({
+          'background-position-y': '-' + wElDifferent2 / 4 + 'px',
+        });
+      }
+
+      if (wBottom >= elTop3) {
+        block3.css({
+          'background-position-y': wElDifferent3 + 'px',
+        });
+      }
 });
