ffc67b93997262d73e5ef78e7218bcb4c4daa363
Ruslan Koshevoy <ruslan.koshevoj@gmail.com>
2017-11-17 10:28:02 +0200

    separated js

--- js/main.js
+++ js/main.js
@@ -1,21 +1,3 @@
-var preloader = document.getElementById("preloader");
-
-function fadeOutnojquery(el) {
-  el.style.opacity = 1;
-  var intervalPreloader = setInterval(function() {
-    el.style.opacity = el.style.opacity - 0.05;
-    if (el.style.opacity <= 0.05) {
-      clearInterval(intervalPreloader);
-      preloader.style.display = "none";
-    }
-  }, 16);
-}
-window.onload = function() {
-  setTimeout(function() {
-    fadeOutnojquery(preloader);
-  }, 500);
-};
-
 $(window).scroll(function() {
   var block2 = $('#slidingSlow2'),
       block3 = $('#slidingSlow3'),
---
+++ js/preloader.js
@@ -0,0 +1,17 @@
+var preloader = document.getElementById("preloader");
+
+function fadeOutnojquery(el) {
+  el.style.opacity = 1;
+  var intervalPreloader = setInterval(function() {
+    el.style.opacity = el.style.opacity - 0.05;
+    if (el.style.opacity <= 0.05) {
+      clearInterval(intervalPreloader);
+      preloader.style.display = "none";
+    }
+  }, 16);
+}
+window.onload = function() {
+  setTimeout(function() {
+    fadeOutnojquery(preloader);
+  }, 500);
+};
